89703971206ebe4a3193ecfe2d69879be3020eb8
Kyle <mbio.kyle@gmail.com>
2016-02-11 16:26:12 -0600

    point the psql url in the right place

--- endorse/config.py
+++ endorse/config.py
@@ -2,7 +2,7 @@ import os
 
 DEBUG = True
 SECRET_KEY = 'sdua98s798asuda98q98SD(A*as'
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.environ.get('ENDORSE_URL')
 
 if SQLALCHEMY_DATABASE_URI == None:
     SQLALCHEMY_DATABASE_URI = "postgresql://localhost/endorse"
